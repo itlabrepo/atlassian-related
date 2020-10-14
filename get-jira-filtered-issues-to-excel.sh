6e117a4f253bb4ed6e7b5c7bea4c27e4b72a1916
itlabrepo <arthur.luox@gmail.com>
2020-10-14 13:24:00 +1000

    Update get-jira-filtered-issues-to-excel.sh
    Add the sample exporting all fields;
    Add comments;
    Add more references

--- get-jira-filtered-issues-to-excel.sh
+++ get-jira-filtered-issues-to-excel.sh
@@ -1,7 +1,12 @@
-curl -u username -o test34424.xls -X GET  https://<<servername>>/sr/jira.issueviews:searchrequest-excel-current-fields/<<filterid>>/SearchRequest-<<filterid>>.xls?tempMax=5000
+# Current fields
+curl -u username -o filename.xls -X GET  https://<<servername>>/sr/jira.issueviews:searchrequest-excel-current-fields/<<filterid>>/SearchRequest-<<filterid>>.xls?tempMax=5000
 
+# All fields
+curl -u username -o filename.xls -X GET  https://<<servername>>/sr/jira.issueviews:searchrequest-excel-all-fields/<<filterid>>/SearchRequest-<<filterid>>.xls?tempMax=5000
 
 # reference
 # https://community.atlassian.com/t5/Jira-questions/How-can-I-periodically-email-the-Excel-version-of-a-report/qaq-p/43247
 # https://developer.atlassian.com/server/jira/platform/jira-rest-api-examples/#searching-for-issues-examples
 # https://stackoverflow.com/questions/44173930/wget-to-download-excel-from-jira-automatically
+# https://confluence.atlassian.com/jiracoreserver089/advanced-searching-1005347631.html
+# https://confluence.atlassian.com/jiracoreserver089/saving-your-search-as-a-filter-1005347649.html
