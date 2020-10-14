6a54661f0765d66378fe7f45b3ed21d9bbd551cd
itlabrepo <arthur.luox@gmail.com>
2020-10-14 10:55:22 +1000

    Create get-jira-filtered-issues-to-excel.sh

---
+++ get-jira-filtered-issues-to-excel.sh
@@ -0,0 +1,5 @@
+curl -u username -o test34424.xls -X GET  https://<<servername>>/sr/jira.issueviews:searchrequest-excel-current-fields/<<filterid>>/SearchRequest-<<filterid>>.xls?tempMax=5000
+
+
+# reference
+# https://community.atlassian.com/t5/Jira-questions/How-can-I-periodically-email-the-Excel-version-of-a-report/qaq-p/43247
