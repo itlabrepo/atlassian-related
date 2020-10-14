337e5bf6b414864d42df39f47b77f85833d6976b
itlabrepo <arthur.luox@gmail.com>
2020-10-14 11:00:17 +1000

    Update get-jira-filtered-issues-to-excel.sh
    add more reference

--- get-jira-filtered-issues-to-excel.sh
+++ get-jira-filtered-issues-to-excel.sh
@@ -3,3 +3,5 @@ curl -u username -o test34424.xls -X GET  https://<<servername>>/sr/jira.issuevi
 
 # reference
 # https://community.atlassian.com/t5/Jira-questions/How-can-I-periodically-email-the-Excel-version-of-a-report/qaq-p/43247
+# https://developer.atlassian.com/server/jira/platform/jira-rest-api-examples/#searching-for-issues-examples
+# https://stackoverflow.com/questions/44173930/wget-to-download-excel-from-jira-automatically
